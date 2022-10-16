222fd61d3457189d6fd199b3fbe5e668460a61dc
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2022-10-16 00:10:15 -0300

    Criar header, footer e baseUrl

---
+++ src/pages/Menu/index.tsx
@@ -0,0 +1,15 @@
+import styles from './Menu.module.scss';
+import {ReactComponent as Logo} from 'assets/logo.svg';
+
+const Menu = () => {
+    return (
+        <main>
+            <nav className={styles.Menu}>
+                <Logo />
+            </nav>
+            <
+        </main>
+    )
+}
+
+export default Menu;
